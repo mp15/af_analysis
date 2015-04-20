8513423bae9e35f3bca0be2f7d0031e76dcc23d1
Martin O. Pollard <mp15@sanger.ac.uk>
2015-04-20 13:13:59 +0100

    Fix analysis script and move to .sh not .pl

--- analysis.pl
+++ analysis.pl
@@ -1,23 +1,4 @@
 my $input_1 = $ARGV[0];
 my $input_2 = $ARGV[1];
 
-# Given that we only have lines with called sites
-# get lines where EUR_AF is set
-#my $cmd = "bcftools query -i'EUR_AF!=0' -f'%INFO/EUR_AF\n' $input_1 > ${input_1}.eur_af_dump";
-#system($cmd);
-#$cmd = "bcftools query -i'EUR_AF!=0' -f'%INFO/EUR_AF\n' $input_2 > ${input_2}.eur_af_dump";
-#system($cmd);
-
-# Now dump all the allele frequencies
-#$cmd = "bcftools query -i'AF!=0' -f'%INFO/AF\n' $input_1 > ${input_1}.af_dump";
-#system($cmd);
-#$cmd = "bcftools query -i'AF!=0' -f'%INFO/AF\n' $input_2 > ${input_2}.af_dump"
-#system($cmd);
-
-#now plot the histogram to show AF distribution
-#$cmd = "/software/R-3.0.0/bin/Rscript analysis.R ${input_1}.eur_af_dump ${input_2}.eur_af_dump eur_af.png";
-#system($cmd);
-#$cmd = "/software/R-3.0.0/bin/Rscript analysis.R ${input_1}.af_dump ${input_2}.af_dump af.png";
-#system($cmd);
-
-system("/software/R-3.0.0/bin/Rscript analysis_bstat.R  <(grep '^AF' ${input_1}.bstat) <(grep '^AF' ${input_2}.bstat)");
+system("./analysis.sh ${input_1} ${input_2}");
---
+++ analysis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+input_1=$1;
+input_2=$2;
+
+# Given that we only have lines with called sites
+# get lines where EUR_AF is set
+#bcftools query -i'EUR_AF!=0' -f'%INFO/EUR_AF\n' $input_1 > ${input_1}.eur_af_dump
+#bcftools query -i'EUR_AF!=0' -f'%INFO/EUR_AF\n' $input_2 > ${input_2}.eur_af_dump
+
+# Now dump all the allele frequencies
+#bcftools query -i'AF!=0' -f'%INFO/AF\n' $input_1 > ${input_1}.af_dump";
+#bcftools query -i'AF!=0' -f'%INFO/AF\n' $input_2 > ${input_2}.af_dump
+
+#now plot the histogram to show AF distribution
+#/software/R-3.0.0/bin/Rscript analysis.R ${input_1}.eur_af_dump ${input_2}.eur_af_dump eur_af.png
+#/software/R-3.0.0/bin/Rscript analysis.R ${input_1}.af_dump ${input_2}.af_dump af.png
+
+temp1=`mktemp af_1XXXX`;
+temp2=`mktemp af_2XXXX`;
+
+grep '^AF' ${input_1}.bstat > $temp1
+grep '^AF' ${input_1}.bstat > $temp2
+
+/software/R-3.0.0/bin/Rscript analysis_bstat.R $temp1 $temp2 bstat.png
+rm $temp1 $temp2
