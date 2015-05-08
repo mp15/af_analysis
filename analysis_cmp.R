62d6e71d7f325acffc52f64acc7d9ee88234c253
Martin O. Pollard <mp15@sanger.ac.uk>
2015-05-08 19:45:52 +0100

    Visual tweaks to r squared and ac plots

--- analysis_cmp.R
+++ analysis_cmp.R
@@ -4,9 +4,14 @@ options <- commandArgs(trailingOnly = TRUE);
 
 # GC[si]S  [2]id   [3]sample       [4]non-reference discordance rate       [5]RR Hom matches       [6]RA Het matches       [7]AA Hom matches       [8]RR Hom mismatches    [9]RA Het mismatches    [10]AA Hom mismatches	[11] dosage r-squared
 
-concord <- read.table(options[1], header=F, col.names=c(rep('',2),'sample','nrdr','RR Hom matches','RA Het matches','AA Hom matches','RR Hom mismatches','RA Het mismatches','AA Hom mismatches','dosage_r'), colClasses=c(rep('NULL',2),'character',rep('numeric',8)));
+concord_snp <- read.table(options[1], header=F, col.names=c(rep('',2),'sample','nrdr','RR Hom matches','RA Het matches','AA Hom matches','RR Hom mismatches','RA Het mismatches','AA Hom mismatches','dosage_r'), colClasses=c(rep('NULL',2),'character',rep('numeric',8)));
+concord_snp$variant_type <- 'SNP';
+concord_indel <- read.table(options[2], header=F, col.names=c(rep('',2),'sample','nrdr','RR Hom matches','RA Het matches','AA Hom matches','RR Hom mismatches','RA Het mismatches','AA Hom mismatches','dosage_r'), colClasses=c(rep('NULL',2),'character',rep('numeric',8)));
+concord_indel$variant_type <- 'INDEL';
+
+concord <-rbind(concord_snp, concord_indel);
 
 palette <- c("#0072B2", "#D55E00");
-cn_plot <- ggplot(data=concord, aes(x=c('100 samples'),y=dosage_r)) + geom_boxplot(orientation="horizontal") + scale_fill_manual(values=palette) +
-     scale_y_continuous(name="dosage r squared") + scale_x_discrete(name="");
-ggsave(filename=options[2],plot=cn_plot);
+cn_plot <- ggplot(data=concord, aes(x=variant_type,y=dosage_r)) + geom_boxplot(orientation="horizontal") + scale_fill_manual(values=palette) +
+     scale_y_continuous(name="Dosage r squared") + scale_x_discrete(name="Variant Type");
+ggsave(filename=options[3],plot=cn_plot);
--- analysis_cmp.sh
+++ analysis_cmp.sh
@@ -7,10 +7,9 @@ temp1=`mktemp GCsS_XXXX`;
 temp2=`mktemp GCiS_XXXX`;
 
 grep '^GCsS' ${input_1} > $temp1
-Rscript analysis_cmp.R $temp1 ${output_prefix}_snp.png
-
 grep '^GCiS' ${input_1} > $temp2
-Rscript analysis_cmp.R $temp2 ${output_prefix}_indel.png
+
+Rscript analysis_cmp.R $temp1 $temp2 ${output_prefix}.png
 
 rm $temp1 $temp2
 
